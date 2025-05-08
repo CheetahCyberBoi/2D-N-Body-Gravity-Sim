3af8432240bd09e45c59b15b4035a72311110842
BuildTools <unconfigured@null.spigotmc.org>
2025-05-07 18:24:53 -0700

    rudimentary non-functioning gravity shader pls fix

--- compute_example.glsl
+++ compute_example.glsl
@@ -6,11 +6,52 @@ layout(set = 0, binding = 0, std430) restrict buffer VelocityBuffer {
     vec2 velocities[];
 } velocities;
 
+// Mass buffer
+layout(set = 0, binding = 1, std430) restrict buffer MassBuffer {
+    float masses[];
+} masses;
+
+// Params buffer
+layout(set = 0, binding = 2, std430) restrict buffer ParamsBuffer {
+    float big_G;
+    float number_of_bodies;
+    float delta;
+    float safety; // Needed to prevent divides by zero
+} params;
+
+layout(set = 0, binding = 3, std430) restrict buffer PositionsBuffer {
+    vec2 positions[];
+}
+
 // Invocations in (x,y,z)
 layout(local_size_x = 8, local_size_y = 8, local_size_z = 8) in;
 
 void main() {
     uint index = gl_GlobalInvocationID.x;
     if (index >= velocities.velocities.length()) return;
-    velocities.velocities[index] *= 2.0;
+    // Early return
+    if (masses.masses[index] < 0.0) {
+        return;
+    }
+
+    // Gravity
+    if (delta > 0.0) {
+        vec3 temp = vec3(0.0);
+
+        // Go through all other bodies
+        for (int other_index = 0; other_index < number_of_bodies; other_index++) {
+            // Skip ourselves
+            if (other_index == index) return;
+
+            // If no mass is encountered, we just break.
+            if (masses.masses[other_index] == 0.0) { break; }
+
+            vec3 diff = positions.positions[other_index] - positions.positions[index];
+            temp += normalize(diff) * masses.masses[other_index] / (dot(diff, diff));
+        }
+
+        // Update data
+        velocities.velocities[index] += vec3(temp * big_g * delta);
+        positions.positions[index] += velocities.velocities[index] * delta;
+    }
 }
--- compute_manager.gd
+++ compute_manager.gd
@@ -9,10 +9,16 @@ extends Node
 @export var max_y_from_center = 500
 
 @export var planet_scene: PackedScene
+# The value of Big G SANS THE EXPONENT
+@export var big_g = 6.67430
+# The safety value times 1E-5.
+@export var safety = 100
 
 
 # The velocities of all bodies thus far.
+var positions: Array = []
 var velocities: Array = []
+var masses: Array = []
 # The actual list of instantiated scenes of the bodies created thus far
 var bodies = []
 
@@ -23,15 +29,16 @@ var pipeline: RID
 
 # Compute shader parameters
 const work_group_size : int = 8
-const num_waitframes_gpusync : int = 12
+const num_waitframes_gpusync : int = 120
 
 # Buffers
 var buffer_set: RID # The collection of buffers (needed for more than one buffer in a single shader
-var velocity_buffer: RID
+var positions_buffer: RID # The buffer containing the positions of all objects.
+var velocity_buffer: RID # The buffr containing the velocities of all objects.
+var mass_buffer: RID # The buffer containing the mass of all objects.
+var params_buffer: RID # The buffer containing parameters specific to the simulation, like big G and similar values.
 
 
-# Outputs from our compute shader
-var output_velocity_buffer: RID
 
 # State
 var frame : int
@@ -67,20 +74,41 @@ func init_compute():
 	var shader_file := load("res://compute_example.glsl")
 	var shader_spirv: RDShaderSPIRV = shader_file.get_spirv()
 	shader = rendering_device.shader_create_from_spirv(shader_spirv)
-	# Setup our buffer to send to the GPU
-	var packed_velocities = PackedVector2Array(velocities)
-	var packed_velocities_bytes = packed_velocities.to_byte_array()
+	# Setup our buffers to send to the GPU
+	var packed_positions = PackedVector2Array(positions).to_byte_array()
+	var packed_velocities = PackedVector2Array(velocities).to_byte_array()
+	var packed_masses = PackedFloat32Array(masses).to_byte_array()
+	var packed_params = PackedFloat32Array(create_params_array()).to_byte_array()
 	
 	# Create a storage buffer to hold everything.
-	velocity_buffer = rendering_device.storage_buffer_create(packed_velocities_bytes.size(), packed_velocities_bytes)
+	positions_buffer = rendering_device.storage_buffer_create(packed_positions.size(), packed_positions)
+	velocity_buffer = rendering_device.storage_buffer_create(packed_velocities.size(), packed_velocities)
+	mass_buffer = rendering_device.storage_buffer_create(packed_masses.size(), packed_masses)
+	params_buffer = rendering_device.storage_buffer_create(packed_params.size(), packed_params)
 	# And assign a uniform to it for the shader.
+	var positions_uniform := RDUniform.new()
+	positions_uniform.uniform_type = RenderingDevice.UNIFORM_TYPE_STORAGE_BUFFER
+	positions_uniform.binding = 3 # Should match `binding` in shader file
+	positions_uniform.add_id(positions_buffer)
+	
 	var velocity_uniform := RDUniform.new()
 	velocity_uniform.uniform_type = RenderingDevice.UNIFORM_TYPE_STORAGE_BUFFER
 	velocity_uniform.binding = 0 # Should match `binding` in shader file
 	velocity_uniform.add_id(velocity_buffer)
 	
+	var mass_uniform := RDUniform.new()
+	mass_uniform.uniform_type = RenderingDevice.UNIFORM_TYPE_STORAGE_BUFFER
+	mass_uniform.binding = 1 # Should match `binding` in shader file
+	mass_uniform.add_id(mass_buffer)
+	
+	var params_uniform := RDUniform.new()
+	params_uniform.uniform_type = RenderingDevice.UNIFORM_TYPE_STORAGE_BUFFER
+	params_uniform.binding = 2 # Should match `binding` in shader file
+	params_uniform.add_id(params_buffer)
+	
+	
 	# Create uniform set and pipeline
-	var uniforms = [velocity_uniform]
+	var uniforms = [velocity_uniform, mass_uniform, params_uniform, positions_uniform]
 	buffer_set = rendering_device.uniform_set_create(uniforms, shader, 0) # last parameter must match `set` in shader file
 	# Create pipeline
 	pipeline = rendering_device.compute_pipeline_create(shader)
@@ -88,9 +116,15 @@ func init_compute():
 	
 func run_compute():
 	# Update velocities packed buffer for the compute shader to execute on.
+	var positions_packed_bytes = PackedVector2Array(positions).to_byte_array()
 	var velocities_packed_bytes = PackedVector2Array(velocities).to_byte_array()
+	var masses_packed_bytes = PackedVector2Array(masses).to_byte_array()
+	var params_packed_bytes = PackedVector2Array(create_params_array()).to_byte_array()
+	rendering_device.buffer_update(positions_buffer, 0, positions_packed_bytes.size(), positions_packed_bytes)
 	rendering_device.buffer_update(velocity_buffer, 0, velocities_packed_bytes.size(), velocities_packed_bytes)
-	print("Velocity array pre_compute: %s" % str(velocities))
+	rendering_device.buffer_update(mass_buffer, 0, masses_packed_bytes.size(), masses_packed_bytes)
+	rendering_device.buffer_update(params_buffer, 0, params_packed_bytes.size(), params_packed_bytes)
+	print("Velocity array pre_compute: %s" % str(positions))
 	# Prepare compute list
 	var local_size = 8
 	var groups_x = int(ceil(float(velocities.size()) / float(local_size))) # figure out how many groups we need based on the number of velocities/thread
@@ -109,8 +143,20 @@ func fetch_and_process_compute_data():
 	waiting_for_compute = false
 	# Get the output from the shader
 	var packed_velocites = rendering_device.buffer_get_data(velocity_buffer)
-	velocities = packed_velocites.to_float32_array() 
-	print("Shader computation completed! Data read back from GPU: %s" % str(velocities))
+	velocities = packed_velocites.to_float32_array()
+	var packed_masses = rendering_device.buffer_get_data(mass_buffer)
+	masses = packed_masses.to_float32_array() 
+	var packed_positions = rendering_device.buffer_get_data(positions_buffer)
+	positions = packed_positions.to_float32_array()
+	print("Shader computation completed! Data read back from GPU: %s" % str(positions))
+	
+func create_params_array():
+	var params = []
+	params.append(big_g * 1E-11)
+	params.append(masses.size()) # Number of bodies in simulation
+	params.append(get_physics_process_delta_time()) # Deltatime for physics.
+	params.append(safety * 1E-5) # Needed to prevent divides by zero in the distance calculations.
+	return params
 
 func create_body():
 	# Create random position for bodies upon creation.
@@ -119,12 +165,14 @@ func create_body():
 	# Instantiate scene at that position
 	var new_planet = planet_scene.instantiate()
 	new_planet.position = random_position
-	new_planet.gravity_scale = 0.0
+	new_planet.custom_integrator = true
 	new_planet.lock_rotation = true
 	new_planet.linear_velocity = Vector2(1.0, 1.0)
 	
 	bodies.append(new_planet)
+	positions.append(new_planet.position)
 	velocities.append(new_planet.linear_velocity)
+	masses.append(1.0)
 	add_child(new_planet)
 	print("Body added to list at coords: (%s, %s)" % [random_position.x, random_position.y])
 
